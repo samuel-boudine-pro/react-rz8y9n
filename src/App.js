9b78069f3ad6f432112a60da3ff0d1bce40a575a
samuel-boudine-pro <152878615+samuel-boudine-pro@users.noreply.github.com>
2024-01-16 16:13:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+import UserProfile from './components/UserProfile';
+import UserContext from './contexts/UserContext';
+
+function App() {
+  const [isOnline, setIsOnline] = useState(false);
+  return (
+    <UserContext.Provider
+      value={{ isOnline: isOnline, setIsOnline: setIsOnline }}
+    >
+      <UserProfile />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/UserProfile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useContext } from 'react';
+import UserContext from '../contexts/UserContext';
+
+function UserProfile() {
+  const { isOnline, setIsOnline } = useContext(UserContext);
+  return (
+    <>
+      <div>User is {isOnline ? 'en ligne' : 'hors ligne'}</div>
+
+      <button onClick={() => setIsOnline(!isOnline)}>
+        click to change user status{' '}
+      </button>
+    </>
+  );
+}
+
+export default UserProfile;
